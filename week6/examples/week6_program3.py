a02d4a115bd4ca6322d5cbd3644c2104f9e9b963
David Groves <github@fibrecat.org>
2018-07-03 00:38:38 +0100

    Fixed a few things in week6.

--- week6/examples/week6_program3.py
+++ week6/examples/week6_program3.py
@@ -3,5 +3,4 @@
 from PIL import Image
 
 myimage = Image.open("london.jpg")
-flipped_image = myimage.transpose(Image.FLIP_LEFT_RIGHT)
-flipped_image.show()
+myimage.show()
--- week6/homework/week6_homework1.py
+++ week6/homework/week6_homework1.py
@@ -6,9 +6,9 @@ image = Image.new("1", (500,500), color=1)
 
 draw = ImageDraw.Draw(image)
 
-a = [0,0]
-b = [250,500]
-c = [500,0]
+a = [100,100]
+b = [400,250]
+c = [100,400]
 
 draw.line(a + b)
 draw.line(a + c)
