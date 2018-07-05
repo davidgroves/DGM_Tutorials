2c15a62d340cf00be9b7fc6283150adbb1319c00
David Groves <github@fibrecat.org>
2018-07-05 01:17:06 +0100

    Added extra credit

---
+++ week6/homework/week6_homework_extracredit.py
@@ -0,0 +1,20 @@
+# Week6, Homework5
+
+from PIL import Image, ImageDraw, ImageColor
+
+# Make our image.
+image = Image.new("RGB", (600,400))
+
+# Make a drawing layer so we can draw on the image
+draw = ImageDraw.Draw(image)
+
+# Draw the blue part
+draw.polygon([(0,0), (0,400), (200,400), (200,0)], fill=ImageColor.getrgb("Blue"))
+
+# Draw the white part
+draw.polygon([(200,0), (200,400), (400,400), (400,0)], fill=ImageColor.getrgb("White"))
+
+# Draw the red part
+draw.polygon([(400,0), (400,400), (600,400), (600,0)], fill=ImageColor.getrgb("Red"))
+
+image.show()
