5ff6fc1260e85fbc050283b68ac55884ada21b42
sweet-n-soya <sweetnsoya.ad@gmail.com>
2018-07-08 20:30:02 +0100

    added whitespace to coordinates

--- week6/homework/week6_homework_extracredit.py
+++ week6/homework/week6_homework_extracredit.py
@@ -9,12 +9,12 @@ image = Image.new("RGB", (600,400))
 draw = ImageDraw.Draw(image)
 
 # Draw the blue part
-draw.polygon([(0,0), (0,400), (200,400), (200,0)], fill=ImageColor.getrgb("Blue"))
+draw.polygon([(0, 0), (0, 400), (200, 400), (200, 0)], fill=ImageColor.getrgb("Blue"))
 
 # Draw the white part
-draw.polygon([(200,0), (200,400), (400,400), (400,0)], fill=ImageColor.getrgb("White"))
+draw.polygon([(200, 0), (200, 400), (400, 400), (400, 0)], fill=ImageColor.getrgb("White"))
 
 # Draw the red part
-draw.polygon([(400,0), (400,400), (600,400), (600,0)], fill=ImageColor.getrgb("Red"))
+draw.polygon([(400, 0), (400, 400), (600, 400), (600, 0)], fill=ImageColor.getrgb("Red"))
 
 image.show()
